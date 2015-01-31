1809098f3c2be9278b9b9cb238989e4b72a41462
Masahiro Yano <masayano@bio.titech.ac.jp>
2015-01-31 22:13:45 +0900

    Update get.sh

--- patternHunter/get.sh
+++ patternHunter/get.sh
@@ -2,8 +2,8 @@
 while [ 1 -eq 1 ]
 do
     sleep 5
-    TEST=`ps auxw | grep "python script/getTweet.py" | grep -v "grep"`
-    if [ -n "$TEST" ]; then
+    TEST=`ps auxw | grep "python script/getTweet.py" | grep -v "grep" | wc -l`
+    if [ $TEST = 0 ]; then
         echo `date`" : do \"python script/getTweet.py\""
         python script/getTweet.py
     fi
